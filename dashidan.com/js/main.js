ba7d41362a3c577b159c782ad20f9c604578c923
dashidan <2903872229@qq.com>
2017-10-24 16:37:05 +0800

    1. 优化java 课程布局 2. 加入小屏适配目录 3. 显示关闭目录 4. 去掉页面顶端标签 5. 去掉子目录中的index.由于部分网站推广链接中采用java/basic/index.html, 暂时保留这个. 6. 修改本文链接路径位置 7. 修改bootstrap缩进样式, 移除默认缩进采用自定义所缩进 8. 修改main.js加入js事件, 修改css样式 9. 修改dashdan.css, 自定义文章模板 10. 美化索引页面,加入图书图标

--- dashidan.com/js/main.js
+++ dashidan.com/js/main.js
@@ -6,3 +6,18 @@ function openInNewWindow(url) {
 function openInCurrentWindow(url) {
     window.location.href = url;
 }
+
+/** 小屏幕显示目录*/
+function showIndex() {
+    $("#xs_index").removeClass("hidden-xs");
+    $("#xs_content").addClass("hidden-xs");
+    $("#sx_index_button").removeClass("visible-xs-block");
+    $("#sx_index_button").addClass("hidden-xs");
+}
+
+function hideIndex() {
+    $("#xs_index").addClass("hidden-xs");
+    $("#xs_content").removeClass("hidden-xs");
+    $("#sx_index_button").removeClass("hidden-xs");
+    $("#sx_index_button").addClass("visible-xs-block");
+}
