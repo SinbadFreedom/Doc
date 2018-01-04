7f4e593df47768100bdf02b12ea720de823058c0
dashidan <2903872229@qq.com>
2018-01-04 13:21:58 +0800

    屏蔽java api9 中script.js中

--- dashidan.com/article/java/api9en/script.js
+++ dashidan.com/article/java/api9en/script.js
@@ -29,69 +29,69 @@ var typeSearchIndex;
 var memberSearchIndex;
 var tagSearchIndex;
 function loadScripts(doc, tag) {
-    createElem(doc, tag, 'jquery/jszip/dist/jszip.js');
-    createElem(doc, tag, 'jquery/jszip-utils/dist/jszip-utils.js');
-    if (window.navigator.userAgent.indexOf('MSIE ') > 0 || window.navigator.userAgent.indexOf('Trident/') > 0 ||
-            window.navigator.userAgent.indexOf('Edge/') > 0) {
-        createElem(doc, tag, 'jquery/jszip-utils/dist/jszip-utils-ie.js');
-    }
-    createElem(doc, tag, 'search.js');
-    
-    $.get(pathtoroot + "module-search-index.zip")
-            .done(function() {
-                JSZipUtils.getBinaryContent(pathtoroot + "module-search-index.zip", function(e, data) {
-                    var zip = new JSZip(data);
-                    zip.load(data);
-                    moduleSearchIndex = JSON.parse(zip.file("module-search-index.json").asText());
-                });
-            });
-    $.get(pathtoroot + "package-search-index.zip")
-            .done(function() {
-                JSZipUtils.getBinaryContent(pathtoroot + "package-search-index.zip", function(e, data) {
-                    var zip = new JSZip(data);
-                    zip.load(data);
-                    packageSearchIndex = JSON.parse(zip.file("package-search-index.json").asText());
-                });
-            });
-    $.get(pathtoroot + "type-search-index.zip")
-            .done(function() {
-                JSZipUtils.getBinaryContent(pathtoroot + "type-search-index.zip", function(e, data) {
-                    var zip = new JSZip(data);
-                    zip.load(data);
-                    typeSearchIndex = JSON.parse(zip.file("type-search-index.json").asText());
-                });
-            });
-    $.get(pathtoroot + "member-search-index.zip")
-            .done(function() {
-                JSZipUtils.getBinaryContent(pathtoroot + "member-search-index.zip", function(e, data) {
-                    var zip = new JSZip(data);
-                    zip.load(data);
-                    memberSearchIndex = JSON.parse(zip.file("member-search-index.json").asText());
-                });
-            });
-    $.get(pathtoroot + "tag-search-index.zip")
-            .done(function() {
-                JSZipUtils.getBinaryContent(pathtoroot + "tag-search-index.zip", function(e, data) {
-                    var zip = new JSZip(data);
-                    zip.load(data);
-                    tagSearchIndex = JSON.parse(zip.file("tag-search-index.json").asText());
-                });
-            });
-    if (!moduleSearchIndex) {
-        createElem(doc, tag, 'module-search-index.js');
-    }
-    if (!packageSearchIndex) {
-        createElem(doc, tag, 'package-search-index.js');
-    }
-    if (!typeSearchIndex) {
-        createElem(doc, tag, 'type-search-index.js');
-    }
-    if (!memberSearchIndex) {
-        createElem(doc, tag, 'member-search-index.js');
-    }
-    if (!tagSearchIndex) {
-        createElem(doc, tag, 'tag-search-index.js');
-    }
+    // createElem(doc, tag, 'jquery/jszip/dist/jszip.js');
+    // createElem(doc, tag, 'jquery/jszip-utils/dist/jszip-utils.js');
+    // if (window.navigator.userAgent.indexOf('MSIE ') > 0 || window.navigator.userAgent.indexOf('Trident/') > 0 ||
+    //         window.navigator.userAgent.indexOf('Edge/') > 0) {
+    //     createElem(doc, tag, 'jquery/jszip-utils/dist/jszip-utils-ie.js');
+    // }
+    // createElem(doc, tag, 'search.js');
+    //
+    // $.get(pathtoroot + "module-search-index.zip")
+    //         .done(function() {
+    //             JSZipUtils.getBinaryContent(pathtoroot + "module-search-index.zip", function(e, data) {
+    //                 var zip = new JSZip(data);
+    //                 zip.load(data);
+    //                 moduleSearchIndex = JSON.parse(zip.file("module-search-index.json").asText());
+    //             });
+    //         });
+    // $.get(pathtoroot + "package-search-index.zip")
+    //         .done(function() {
+    //             JSZipUtils.getBinaryContent(pathtoroot + "package-search-index.zip", function(e, data) {
+    //                 var zip = new JSZip(data);
+    //                 zip.load(data);
+    //                 packageSearchIndex = JSON.parse(zip.file("package-search-index.json").asText());
+    //             });
+    //         });
+    // $.get(pathtoroot + "type-search-index.zip")
+    //         .done(function() {
+    //             JSZipUtils.getBinaryContent(pathtoroot + "type-search-index.zip", function(e, data) {
+    //                 var zip = new JSZip(data);
+    //                 zip.load(data);
+    //                 typeSearchIndex = JSON.parse(zip.file("type-search-index.json").asText());
+    //             });
+    //         });
+    // $.get(pathtoroot + "member-search-index.zip")
+    //         .done(function() {
+    //             JSZipUtils.getBinaryContent(pathtoroot + "member-search-index.zip", function(e, data) {
+    //                 var zip = new JSZip(data);
+    //                 zip.load(data);
+    //                 memberSearchIndex = JSON.parse(zip.file("member-search-index.json").asText());
+    //             });
+    //         });
+    // $.get(pathtoroot + "tag-search-index.zip")
+    //         .done(function() {
+    //             JSZipUtils.getBinaryContent(pathtoroot + "tag-search-index.zip", function(e, data) {
+    //                 var zip = new JSZip(data);
+    //                 zip.load(data);
+    //                 tagSearchIndex = JSON.parse(zip.file("tag-search-index.json").asText());
+    //             });
+    //         });
+    // if (!moduleSearchIndex) {
+    //     createElem(doc, tag, 'module-search-index.js');
+    // }
+    // if (!packageSearchIndex) {
+    //     createElem(doc, tag, 'package-search-index.js');
+    // }
+    // if (!typeSearchIndex) {
+    //     createElem(doc, tag, 'type-search-index.js');
+    // }
+    // if (!memberSearchIndex) {
+    //     createElem(doc, tag, 'member-search-index.js');
+    // }
+    // if (!tagSearchIndex) {
+    //     createElem(doc, tag, 'tag-search-index.js');
+    // }
     $(window).resize(function() {
         $('.navPadding').css('padding-top', $('.fixedNav').css("height"));
     });
