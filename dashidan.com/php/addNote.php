c815cd50a0ac58e1b5c1f7604328276c83f4143e
SinbadFreedom <151229152@qq.com>
2019-03-20 02:59:50 +0800

    1. 修改mongodb入库 和读取 测试逻辑

--- dashidan.com/php/addNote.php
+++ dashidan.com/php/addNote.php
@@ -5,25 +5,24 @@
  * Date: 2019/3/20
  * Time: 1:03
  */
-$docNum = $_POST['num'];
+$num = $_POST['num'];
 $note = $_POST['note'];
 
 //TODO check num is number format
 
-if (!$docNum || !$note) {
+if (!$num || !$note) {
     echo 'param error';
     return;
 }
 
-$db_name = 'python3';
+/** collection name*/
+$db_collection_name = 'python3.note_' . $num;
 
 $manager = new MongoDB\Driver\Manager('mongodb://localhost:27017');
 $bulk = new MongoDB\Driver\BulkWrite;
-$bulk->insert(['num' => $docNum, 'note' => $note]);
+$bulk->insert(['num' => $num, 'note' => $note]);
 
 $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 3000);//可选，修改确认
-$res = $manager->executeBulkWrite($db_name, $bulk, $writeConcern);
+$res = $manager->executeBulkWrite($db_collection_name, $bulk, $writeConcern);
 //echo '<pre>';
-print_r($res);
-
-//echo 'docNum: ' . $docNum . " " . " note: " . $note;
+//print_r($res);
--- dashidan.com/php/getNote.php
+++ dashidan.com/php/getNote.php
@@ -12,19 +12,24 @@ if (!$num) {
     return;
 }
 
-$db_name = 'python3';
+/** collection name*/;
+$db_collection_name = 'python3.note_' . $num;
 
 $manager = new MongoDB\Driver\Manager('mongodb://localhost:27017');
 $query = new MongoDB\Driver\Query(['num' => $num]);
-$cursor = $manager->executeQuery($db_name, $query);
+$cursor = $manager->executeQuery($db_collection_name, $query);
 
 $data = [];
-foreach($cursor as $doc) {
-    $data[] = $doc;
+foreach ($cursor as $doc) {
+    array_push($data, $doc);
 }
 
 print_r($data);
 
-$info = $data[0];
-
-echo json_encode($info);
+//TODO 笔记数据分页
+if (sizeof($data) > 0) {
+    $info = $data[0];
+    echo json_encode($info);
+} else {
+    echo "{}";
+}
