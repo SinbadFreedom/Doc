9a0d16b08e3b791207800f6c95132f6ceb8727cf
SinbadFreedom <151229152@qq.com>
2019-03-20 01:05:39 +0800

    1. 加入 addNote.php 测试

---
+++ dashidan.com/php/addNote.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinbad
+ * Date: 2019/3/20
+ * Time: 1:03
+ */
+$docNum = $_POST['num'];
+$note = $_POST['note'];
+
+echo 'docNum: ' . $docNum . " " . " note: " . $note;
