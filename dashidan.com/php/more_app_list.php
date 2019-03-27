a8d5021d9426dc13839aa6ffcf3cd1b3fb615f4c
SinbadFreedom <151229152@qq.com>
2019-03-27 10:10:52 +0800

    1. 修改 笔记 相关的文件名改为小写 note_add.php, note_get.php 2. 增加more_app_list.php和rank_list.php 模板

---
+++ dashidan.com/php/more_app_list.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinbad
+ * Date: 2019/3/27
+ * Time: 10:03
+ */
+
+echo "Hello more app list.";
---
+++ dashidan.com/php/rank_list.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sinbad
+ * Date: 2019/3/27
+ * Time: 10:02
+ */
+
+echo "Hello rank list.";
