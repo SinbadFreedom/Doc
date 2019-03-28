fa494943c9afb7f79fbe9548d824a1bd4b69ae43
SinbadFreedom <151229152@qq.com>
2019-03-28 11:47:19 +0800

    1. 加入upload_img

--- dashidan.com/php/upload_img.php
+++ dashidan.com/php/upload_img.php
@@ -6,14 +6,12 @@
  * Time: 10:41
  */
 
-echo "----------------------1";
 if (isset($_POST['openid'])) {
     $openid = $_POST['openid'];
 } else {
     echo "param error 0";
     return;
 }
-echo "----------------------2";
 if (isset($_POST['imgdata'])) {
     $img_data = $_POST['imgdata'];
 } else {
@@ -21,21 +19,9 @@ if (isset($_POST['imgdata'])) {
     return;
 }
 
-echo "----------------------3";
 $decoded_image = base64_decode($img_data);
-echo "----------------------4";
+/** 需要提前创建好 上级 head_img目录，并且设置目录权限 777 */
 $file_name = "../head_img/" . $openid . ".jpg";
 $return = file_put_contents($file_name, $decoded_image);
-echo "----------------------5";
-var_dump($return);
-echo "----------------------6";
 
-//getimagesize($file_name);
-//$imageInfo = getimagesize($imageName);
-//if ($imageInfo !== false) {
-//    $imageType = strtolower(substr(image_type_to_extension($imageInfo [2]), 1));
-//    $imageSize = filesize($imageInfo);
-//    return $info = array('width' => $imageInfo [0], 'height' => $imageInfo [1], 'type' => $imageType, 'size' => $imageSize, 'mine' => $imageInfo ['mine']);
-//} else {
-//    //不是合法的图片
-//    return null;
+var_dump($return);
